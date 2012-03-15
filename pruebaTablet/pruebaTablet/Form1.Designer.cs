d7ff6a4c76acca695ea79ea57a7715e14969e36d
Joan Pastor <jpastor@VMWin7.(none)>
2012-03-15 17:58:15 +0100

    Add Gesture Features to the code

--- pruebaTablet/pruebaTablet/Form1.Designer.cs
+++ pruebaTablet/pruebaTablet/Form1.Designer.cs
@@ -33,11 +33,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(264, 247);
+            this.button1.Location = new System.Drawing.Point(69, 243);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Recognition";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
--- pruebaTablet/pruebaTablet/Form1.cs
+++ pruebaTablet/pruebaTablet/Form1.cs
@@ -15,6 +15,7 @@ namespace pruebaTablet
         public Form1()
         {
             InitializeComponent();
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -22,7 +23,13 @@ namespace pruebaTablet
             inkOverlay = new InkOverlay();
             inkOverlay.Handle = this.Handle;
             inkOverlay.Enabled = true;
-            
+            inkOverlay.Gesture += new InkCollectorGestureEventHandler(Event_OnApplicationGesture);
+            inkOverlay.CollectionMode = CollectionMode.InkAndGesture;
+            inkOverlay.SetGestureStatus(ApplicationGesture.Left, true);
+            inkOverlay.SetGestureStatus(ApplicationGesture.Right, true);
+            inkOverlay.SetGestureStatus(ApplicationGesture.Up, true);
+            inkOverlay.SetGestureStatus(ApplicationGesture.Down, true);
+            inkOverlay.SetGestureStatus(ApplicationGesture.Check,true);
             
         }
 
@@ -44,5 +51,40 @@ namespace pruebaTablet
             }
 
         }
+
+        void Event_OnApplicationGesture(object sender, InkCollectorGestureEventArgs e)
+        {
+            Gesture G = e.Gestures[0];
+
+            // we will use the gesture if it has confidence of strong or intermediate
+
+            if (G.Confidence == RecognitionConfidence.Intermediate ||
+                G.Confidence == RecognitionConfidence.Strong)
+            {
+
+                switch (G.Id)
+                {
+                    case ApplicationGesture.Left:
+                        MessageBox.Show("Left");
+                        break;
+                    case ApplicationGesture.Right:
+                        MessageBox.Show("Right");
+                        break;
+                    case ApplicationGesture.Up:
+                        MessageBox.Show("Up");
+                        break;
+                    case ApplicationGesture.Down:
+                        MessageBox.Show("Down");
+                        break;
+                    case ApplicationGesture.Check:
+                        MessageBox.Show("Check");
+                        break;
+                }
+            }
+
+        }
+
+
+ 
     }
 }
